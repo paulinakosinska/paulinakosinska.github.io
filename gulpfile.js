26d58bacde9ce4a20deabd1bff887936dfdcd6d3
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-01-18 21:29:37 +0100

    add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task("scss", function () {
     return gulp.src('app/scss/*.scss')
         .pipe(sass()) 
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+          }))
 }); 
 
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync'], function(){
     gulp.watch('app/scss/**/*.scss', ['scss']); 
   });
+
+  gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+  }); 
