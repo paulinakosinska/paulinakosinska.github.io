ccf3f42df928a11fdf54d95510a758ebfb26750b
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-01-18 21:20:07 +0100

    add "watch" task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var sass = require('gulp-sass');
 
 gulp.task("scss", function () {
     return gulp.src('app/scss/*.scss')
-        .pipe(sass()) // Using gulp-sass
+        .pipe(sass()) 
         .pipe(gulp.dest('app/css'))
-});
+}); 
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', ['scss']); 
+  });
