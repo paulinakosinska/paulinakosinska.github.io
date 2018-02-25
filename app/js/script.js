a022244b0685411635944ed35e6c4ed98f1367ab
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-02-25 18:29:05 +0100

    change classes in navbar-logo, add style and js events

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,17 @@
 $(window).scroll(function () {
-    if ($(".navbar").offset().top > 60) {
-        $(".navbar").addClass('navbar-scrolled');
-        $("#navbar-logo").css("display", "inline");
+    if ($('.navbar').offset().top > 60) {
+        $('.navbar').addClass('navbar-scrolled');
+        $('#navbar-logo').removeClass("hidden");
     } else {
-        $(".navbar").removeClass('navbar-scrolled');
-        $("#navbar-logo").css("visibility", "hidden");
+        $('.navbar').removeClass('navbar-scrolled');
+        $('#navbar-logo').addClass("hidden");
     }
 });
+
+$('#navbarNav').on('show.bs.collapse', function () {
+    $('nav').toggleClass("navbar-scrolled");
+})
+
+$('.nav-link').on('click', function () {
+    $('#navbarNav').collapse('hide');
+})
