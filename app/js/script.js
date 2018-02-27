06d1c26519e6855e77e5efc5c43054de50bc122e
Paulina  Kosińska <paulinakosinska95@gmail.com>
2018-02-27 10:29:10 +0100

    underline nav-links

--- app/js/script.js
+++ app/js/script.js
@@ -32,3 +32,26 @@ $('a[href^="#"]').on('click', function (event) {
 });
 
 AOS.init();
+
+$(window).scroll(function () {
+    const titleWraperOffset = $('#title-wraper').offset().top;
+    const aboutMeOffset = $('#about-me').offset().top;
+    const skillsOffset = $('#skills').offset().top;
+    const projectsOffset = $('#projects').offset().top;
+    const contactMeOffset = $('#contact-me').offset().top;
+    let currentPosition = document.documentElement.scrollTop +1;
+    
+    $('a[href^="#"]').removeClass("underline-link");
+    
+    if (currentPosition >= titleWraperOffset && currentPosition <= aboutMeOffset) {
+        $('a[href$="#title-wraper"]').addClass("underline-link");
+    } else if (currentPosition >= aboutMeOffset && currentPosition <= skillsOffset) {
+        $('a[href$="#about-me"]').addClass("underline-link");
+    } else if (currentPosition >= skillsOffset && currentPosition <= projectsOffset) {
+        $('a[href$="#skills"]').addClass("underline-link");
+    }else if (currentPosition >= projectsOffset && currentPosition <= contactMeOffset) {
+        $('a[href$="#projects"]').addClass("underline-link");
+    } else if (currentPosition >= contactMeOffset) {
+        $('a[href$="#contact-me"]').addClass("underline-link");
+    }
+});
